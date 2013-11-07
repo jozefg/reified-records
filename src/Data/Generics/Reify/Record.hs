fd6ae1e0266bd4bb4bda5abe1423307508101574
Danny Gratzer <danny.gratzer@gmail.com>
2013-11-06 22:23:11 -0600

    Fixed naming
    --HG--
    rename : src/Data/Generic/Reify/Record.hs => src/Data/Generics/Reify/Record.hs

--- src/Data/Generics/Reify/Record.hs
+++ src/Data/Generics/Reify/Record.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE Safe #-}
 module Data.Generics.Reify.Record (reify, reflect) where
 import Data.Dynamic
 import Data.Typeable
@@ -14,6 +14,9 @@ fields a = if length cs == 1 && length fs > 0 then Just fs else Nothing
   where cs = dataTypeConstrs . dataTypeOf $ a
         fs = cs >>= constrFields
 
+-- | Reify a @Map@ of strings to an arbitrary type. If the type is a record, each of
+-- its field names will be looked up in the record. If any of the types don't match
+-- or if @a@ isn't a record, @Nothing@ will be returned.
 reify :: Data a => Map String Dynamic -> Maybe a
 reify vault = result
   where constrs     = dataTypeConstrs . dataTypeOf $ fromJust result
@@ -21,5 +24,8 @@ reify vault = result
         result      = flip evalStateT fs . gmapM popFields . fromConstr . head $ constrs
         popFields _ = (get >>= lift . (fromDynamic . head =<<)) <* modify (fmap tail)
 
+-- | If @a@ is a record, this will return a @Map@ where the keys
+-- are the field names and the values are wrapped in @toDyn@. Otherwise
+-- @Nothing@ will be returned.
 reflect :: Data a => a -> Maybe (Map String Dynamic)
 reflect a = fields a >>= return . M.fromList . flip zip (gmapQ toDyn a)
